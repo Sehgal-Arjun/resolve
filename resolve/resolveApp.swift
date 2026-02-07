cfa2b16001416ed8c0eac7c924c3cafd3400e441
Arjun Sehgal <arjunsehgal789@gmail.com>
2026-02-07 01:55:51 -0500

    initial commit

---
+++ resolve/resolveApp.swift
@@ -0,0 +1,17 @@
+//
+//  resolveApp.swift
+//  resolve
+//
+//  Created by Arjun Sehgal on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct resolveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ resolveTests/resolveTests.swift
@@ -0,0 +1,17 @@
+//
+//  resolveTests.swift
+//  resolveTests
+//
+//  Created by Arjun Sehgal on 2026-02-07.
+//
+
+import Testing
+@testable import resolve
+
+struct resolveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
